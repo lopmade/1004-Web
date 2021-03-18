44066933e29a900ddf143b1ebc59a701854b9c10
s0l1tary <theinfiniteboxx@gmail.com>
2021-03-18 10:29:04 +0800

    sample

--- index.php
+++ index.php
@@ -4,22 +4,22 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/main.css">
+
+        <?php
+        include "nav.inc.php";
+        include "footer.inc.php";
+        ?>
+        
     </head>
     <body>
-        <main>
-            <header>
-                <h1>Welcome to Forum for Selling items</h1>
-                <h2>Home of trading</h2>
-                <nav>
-                    <ul>
-                        <li><a href="#dogs">Home</a></li>
-                        <li><a href="#cats">Profile</a></li>
-                    </ul>
-                </nav>
-            
-            <footer>
-                <p><em>Copyright &copy; 2021 Carousell Hub Pte. Ltd.</em></p>
-            </footer>
+        <main class = "main">
+            <section class = "section">
+                <header>
+                    <h1>Welcome to Forum for Selling items</h1>
+                    <h2>Home of trading</h2>
+                </header>
+
+            </section>
         </main>
     </body>
 </html>
---
+++ nav.inc.php
@@ -0,0 +1,6 @@
+<div class="navbar">
+    <a href="#home">Home</a>
+    <a href="#shop">Shop</a>
+    <a href="#Forum">Forum</a>
+    <a href="#contact">Contact Us</a>
+</div>
