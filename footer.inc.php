88315e0f38d0946b07e5a8638ffa32164ad42f3d
s0l1tary <theinfiniteboxx@gmail.com>
2021-03-18 11:09:48 +0800

    sample index page with nav

--- footer.inc.php
+++ footer.inc.php
@@ -1,4 +1,4 @@
 
-<footer class="container">
+<footer class="footer_container">
     <p><em>Copyright &copy; 2021 Carousell Hub Pte. Ltd.</em></p>
 </footer>
--- nav.inc.php
+++ nav.inc.php
@@ -1,6 +1,12 @@
-<div class="navbar">
-    <a href="#home">Home</a>
-    <a href="#shop">Shop</a>
-    <a href="#Forum">Forum</a>
-    <a href="#contact">Contact Us</a>
-</div>
+
+<nav>
+    <ul>
+        <li><img src = "images/Carousell-logo-square.png" width = "50" height = "50" alt = "LOGO"></i>
+        <li><a class="active" href="#home">Home</a></li>
+        <li><a href="#products">Products</a></li>
+        <li><a href="#promotions">Promotions</a></li>
+        <li><a href="#contact">Contact Us</a></li>
+        <li style="float:right"><a href="#register">Register</a></li>
+        <li style="float:right"><a href="#login">Login</a></li>
+    </ul>
+</nav>
