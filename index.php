20dd588527a28c7df36be50dda120ea79b4223f1
s0l1tary <theinfiniteboxx@gmail.com>
2021-03-18 09:56:43 +0800

    sample files for subfolders & changes to index
    css,images,js,

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-
+<html>
+    <head>
+        <title>Carousell Hub</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <main>
+            <header>
+                <h1>Welcome to Forum for Selling items</h1>
+                <h2>Home of trading</h2>
+                <nav>
+                    <ul>
+                        <li><a href="#dogs">Home</a></li>
+                        <li><a href="#cats">Profile</a></li>
+                    </ul>
+                </nav>
+            
+            <footer>
+                <p><em>Copyright &copy; 2021 Carousell Hub Pte. Ltd.</em></p>
+            </footer>
+        </main>
+    </body>
+</html>
