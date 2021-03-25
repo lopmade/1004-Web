25017c6623f9f440cbaa7a5196f090dd72262267
s0l1tary <theinfiniteboxx@gmail.com>
2021-03-25 18:25:47 +0800

    updated css and nav links
    removal of unused css files
    added more web pages for navigation
    bootstrap and css styling

--- nav.inc.php
+++ nav.inc.php
@@ -1,12 +1,36 @@
+<nav id="mainNav" class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid" >
+        <a id="logo" class="navbar-brand" href="index.php">
+            <img src="/images/pokemart_logo.png" alt="Pokemart Logo" width="30" height="30">
+            Pokemart</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="market.php">Marketplace</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="promotions.php">Promotions</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.php">About Us</a>
+                </li>
+            </ul>
 
-<nav>
-    <ul>
-        <li><img src = "images/Carousell-logo-square.png" width = "50" height = "50" alt = "LOGO"></i>
-        <li><a class="active" href="#home">Home</a></li>
-        <li><a href="#products">Products</a></li>
-        <li><a href="#promotions">Promotions</a></li>
-        <li><a href="#contact">Contact Us</a></li>
-        <li style="float:right"><a href="#register">Register</a></li>
-        <li style="float:right"><a href="#login">Login</a></li>
-    </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a class="nav-link" href="register.php">Register</a>
+                </li>
+                <li>
+                    <a class="nav-link" href="login.php">Login</a>
+                </li>
+            </ul>
+
+        </div>
+    </div>
 </nav>
