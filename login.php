38f414ad03fb8c098cdf80bc29944c76829b0f05
c0rekt <jian1996@live.com.sg>
2021-03-30 01:34:50 +0800

    update

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ if (!isset($_SESSION)) {
 }
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-    header("location: welcome.php");
+    header("location: profile.php");
     exit;
 }
 
