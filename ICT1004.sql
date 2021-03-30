cc2d0a67e71c118a9e6d75425a131b0215baf4e5
c0rekt <jian1996@live.com.sg>
2021-03-30 16:25:22 +0800

    token added for email verification

--- ICT1004.sql
+++ ICT1004.sql
@@ -24,6 +24,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`user` (
   `password` VARCHAR(255) NOT NULL,
   `first_name` VARCHAR(255) NOT NULL,
   `last_name` VARCHAR(255) NOT NULL,
+  `token` VARCHAR(255) NOT NULL,
   `user_verified` TINYINT NOT NULL DEFAULT 0,
   PRIMARY KEY (`user_id`),
   UNIQUE INDEX `UserID_UNIQUE` (`user_id` ASC) )
