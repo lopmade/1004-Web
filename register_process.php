1c006d1a5f2aa8384a0982dae60e28d1b6901b86
c0rekt <jian1996@live.com.sg>
2021-03-31 22:30:46 +0800

    Update register_process.php
    fix the weird merge shit

--- register_process.php
+++ register_process.php
@@ -1,17 +1,11 @@
 <?php
 // Include config file
 require_once "config.php";
-<<<<<<< Updated upstream
- 
-=======
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-require 'PHPMailer-master/src/Exception.php';
-require 'PHPMailer-master/src/PHPMailer.php';
-require 'PHPMailer-master/src/SMTP.php';
+require './email_credentials.php';
+require './PHPMailer/src/Exception.php';
+require './PHPMailer/src/PHPMailer.php';
 
->>>>>>> Stashed changes
 // Define variables and initialize with empty values
 $username = $password = $confirm_password = $first_name = $last_name = $email = "";
 $username_err = $password_err = $confirm_password_err = $first_name_err = $last_name_err = $email_err = "";
@@ -96,11 +90,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         mysqli_stmt_close($stmt);
     }
     
+    // Validate password strength
+    $password = $_POST["password"];
+    $uppercase = preg_match('@[A-Z]@', $password);
+    $lowercase = preg_match('@[a-z]@', $password);
+    $number    = preg_match('@[0-9]@', $password);
+    $specialChars = preg_match('@[^\w]@', $password);
+    
     // Validate password
     if(empty(trim($_POST["password"]))){
         $password_err = "Please enter a password.";     
-    } elseif(strlen(trim($_POST["password"])) < 6){
-        $password_err = "Password must have atleast 6 characters.";
+    } elseif(!$uppercase || !$lowercase || !$number || !$specialChars || strlen(trim($password)) < 7){
+        $password_err = "Password must have mininally 8 characters, a lower case, an upper case, a number and a special character.";
     } else{
         $password = trim($_POST["password"]);
     }
@@ -119,12 +120,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err && empty($email_err) && empty($first_name_err) && empty($last_name_err))){
         
         // Prepare an insert statement
-        $sql = "INSERT INTO user (username, password, email, first_name, last_name) VALUES (?, ?, ?, ?, ?)";
+        $sql = "INSERT INTO user (username, password, email, first_name, last_name, token) VALUES (?, ?, ?, ?, ? ,?)";
          
         $stmt = mysqli_prepare($link, $sql);
-<<<<<<< Updated upstream
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "sssss", $param_username, $param_password, $param_email, $param_first_name, $param_last_name);
+            mysqli_stmt_bind_param($stmt, "ssssss", $param_username, $param_password, $param_email, $param_first_name, $param_last_name, $param_token);
             
             // Set parameters
             $param_username = $username;
@@ -132,65 +132,35 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $param_email = $email;
             $param_first_name = $first_name;
             $param_last_name = $last_name;
+            $param_token = md5($param_username . date("dmYhis"));
+            $url = $_SERVER['SERVER_NAME'];
             
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Redirect to login page
                 header("location: login.php");
+                
+                $mail = new PHPMailer(true);
+                try {
+                    $mail->Username   = EMAIL;                          //SMTP username
+                    $mail->Password   = PASS;                           //SMTP password
+                    $mail->setFrom(EMAIL , 'POKESTOP');                 //SEND FROM
+                    $mail->addAddress($param_email, $param_first_name); //SEND TO
+                    
+                    //EMAIL CONTENT
+                    $mail->isHTML(true);
+                    $mail->Subject = 'Regisration verification!';
+                    $mail->Body = '' ;
+                    $mail->AltBody = '';
+
+                echo 'Message has been sent';
+                } catch (Exception $e) {
+                    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                }
+                
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
-=======
-        // Bind variables to the prepared statement as parameters
-        mysqli_stmt_bind_param($stmt, "ssssss", $param_username, $param_password, $param_email, $param_first_name, $param_last_name, $param_token);
-
-        // Set parameters
-        $param_username = $username;
-        $param_password = password_hash($password, PASSWORD_BCRYPT); // Creates a password hash
-        $param_email = $email;
-        $param_first_name = $first_name;
-        $param_last_name = $last_name;
-        $param_token = md5($param_username . date("dmYhis"));
-        $url = $_SERVER['SERVER_NAME'].'verify.php?email='.$email.'&token='.$token;
-
-        // Attempt to execute the prepared statement
-        if(mysqli_stmt_execute($stmt)){
-            // Redirect to login page
-            header("location: login.php");
-
-            $mail = new PHPMailer(true);
-            try {
-                //Initialising
-                $mail->isSMTP();
-                $mail->Mailer = "smtp";
-                //Config
-                $mail->SMTPDebug  = 1;  
-                $mail->SMTPAuth   = TRUE;
-                $mail->SMTPSecure = "tls";
-                $mail->Port       = 587;
-                $mail->Host       = "smtp.gmail.com";
-                $mail->SMTPAuth   = true;
-                $mail->Username   = EMAIL;                          //SMTP username
-                $mail->Password   = PASS;                           //SMTP password
-                
-                $mail->IsHTML(true);
-                $mail->setFrom(EMAIL , 'POKESTOP');                 //SEND FROM
-                $mail->addAddress($email, $first_name);             //SEND TO
-
-                //EMAIL CONTENT
-                $mail->isHTML(true);
-                $mail->Subject = 'Regisration verification!';
-                $mail->Body = $url;
-                $mail->AltBody = $url;
-                echo 'Message has been sent';
-                    
-            } catch (Exception $e) {
-                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
->>>>>>> Stashed changes
             }
-
-        } else{
-            echo "Oops! Something went wrong. Please try again later.";
-        }
 
             // Close statement
             mysqli_stmt_close($stmt);
