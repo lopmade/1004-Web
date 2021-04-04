f06946ec8991ab837570b5fa87145eb65d7c3560
ImpostersSyndrome <thebystanders@outlook.com>
2021-04-04 09:54:38 +0800

    Fixed comment.

--- marketitem.php
+++ marketitem.php
@@ -116,7 +116,7 @@ function goBackToMarket() {
                 <h6>DATE ADDED:<?php echo $date_added; ?></h6>
                 <!--can only display one image at the time for now ,need carousell or something in the future --> 
                 <img src="images/market/<?php echo $item_image ?>" >
-                // Only user's can update or delete its own item.
+                <!--Only user's can update or delete its own item-->
                 <?php if ($user_user_id == $_SESSION['user_id']) { ?>
                     <a class="btn btn-primary" href="update.php?id=<?php echo $_GET['item_id'] ?>" role="button">Edit</a>
                     <a class="btn btn-primary" href="delete.php?id=<?php echo $_GET['item_id'] ?>" name="delete" role="button">Delete</a>
