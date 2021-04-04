13871aeb00e983f9c6a417574894fc56ea22b8c0
ImpostersSyndrome <thebystanders@outlook.com>
2021-04-04 10:06:09 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -11,7 +11,7 @@ if (isset($_POST["id"]) && !empty($_POST["id"])) {
 
     // Set SQL delete query
     $del_item = "delete from items_listing where item_id=?";
-    
+
     // Prepare a delete statement
     if ($run_item = mysqli_prepare($link, $del_item)) {
         // Bind variables to the prepared statement as parameters
@@ -69,24 +69,26 @@ function sanitize_input($data) {
         </style>
     </head>
     <body>
-        <div class="wrapper">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h2 class="mt-5 mb-3">Delete Record</h2>
-                        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                            <div class="alert alert-danger">
-                                <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
-                                <p>Are you sure you want to delete this employee record?</p>
-                                <p>
-                                    <input type="submit" value="Yes" class="btn btn-danger">
-                                    <a href="market.php" class="btn btn-secondary">No</a>
-                                </p>
-                            </div>
-                        </form>
-                    </div>
-                </div>        
-            </div>
-        </div>
+        <main class="main">
+            <section class="wrapper">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h2 class="mt-5 mb-3">Delete Item</h2>
+                            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                                <div class="alert alert-danger">
+                                    <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
+                                    <p>Are you sure you want to delete this Item?</p>
+                                    <p>
+                                        <input type="submit" value="Yes" class="btn btn-danger">
+                                        <a href="market.php" class="btn btn-secondary">No</a>
+                                    </p>
+                                </div>
+                            </form>
+                        </div>
+                    </div>        
+                </div>
+            </section>
+        </main>
     </body>
 </html>
