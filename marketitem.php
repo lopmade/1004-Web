383f3e9846251bf6980dcd620ca1b5120f1dd974
Ezykill <coolzekk@gmail.com>
2021-04-09 20:05:13 +0800

    More fixes

--- marketitem.php
+++ marketitem.php
@@ -79,13 +79,15 @@ function goBackToMarket() {
     die();
 }
 ?>
-<html>
+<html lang="en">
     <head>
         <?php
         include "header.inc.php";
         ?>
+        <link rel="stylesheet" href="css/market.css"/>
         <link rel="stylesheet" 
               href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" >
+
 
         <!-- JavaScript Bundle with Popper -->
         <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js">
@@ -113,7 +115,7 @@ function goBackToMarket() {
                 <h6>DESCRIPTION:<?php echo $description; ?></h6>
                 <h6>DATE ADDED:<?php echo $date_added; ?></h6>
                 <!--can only display one image at the time for now ,need carousell or something in the future --> 
-                <img src="images/market/<?php echo $item_image ?>" >
+                <img alt="item image" src="images/market/<?php echo $item_image ?>" >
                 <!--Only user's can update or delete its own item-->
                 <?php
                 // if the item being viewed is NOT the user's item
