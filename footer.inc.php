e6cb46006a62b3e79083f0c175f02c35b4215f77
Ezykill <coolzekk@gmail.com>
2021-04-09 20:23:13 +0800

    footer fix

--- footer.inc.php
+++ footer.inc.php
@@ -1,5 +1,5 @@
-<section id="space">
+<section class="space">
 </section>
 <footer style="width:100%;"id="footer" class="footer_container text-center mx-auto fixed-bottom ">
-    <p id ="footerText"><em>Copyright &copy; 2021 Pokemart Singapore</em></p>
+    <p id ="footerText"><em style="color:#001329">Copyright &copy; 2021 Pokemart Singapore</em></p>
 </footer>
